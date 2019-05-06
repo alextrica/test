afac3267529057095b89a756ab25340d443646ac
alextrica <trica.alex1998@yahoo.com>
2019-05-06 19:50:10 +0300

    fsaf

--- src/problema2.java
+++ src/problema2.java
@@ -8,7 +8,7 @@ public class problema2{
             if (n % 2 == 0) {
                 n = n / 2;
             } else {
-                n = 3 * n + 1;
+                n = 3 * n+ 1;
             }
             System.out.println("N=" +n);
             counter++;
